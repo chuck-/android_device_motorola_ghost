bfe1eab4af2a1d0e603b296a95f0cc07db3dfa8f
chuck- <chuck-@users.noreply.github.com>
2015-11-06 18:55:49 +0100

    now it boots

--- mr_init_devices.c
+++ mr_init_devices.c
@@ -7,30 +7,38 @@
 // You can use * at the end to init this folder and all its subfolders
 const char *mr_init_devices[] =
 {
-    "/sys/class/graphics/fb0",
+    "/sys/devices/virtual/graphics/fb0",
 
     "/sys/block/mmcblk0",
     "/sys/devices/platform/msm_sdcc.1",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0*",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p33",
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p40",
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p38",
+	
     "/sys/bus/mmc",
     "/sys/bus/mmc/drivers/mmcblk",
     "/sys/module/mmc_core",
-    "/sys/module/mmcblk",
+    "/sys/module/mmcblk",	
 
     "/sys/devices/virtual/input*",
     "/sys/devices/virtual/misc/uinput",
+    "/sys/devices/platform/msm_ssbi.0/pm8921-core/pm8xxx-keypad/input*",
+    "/sys/devices/platform/msm_ssbi.0/pm8921-core/pm8xxx-pwrkey/input*",
 
     // for adb
-    "/sys/class/tty/ptmx",
-    "/sys/class/misc/android_adb",
-    "/sys/class/android_usb/android0/f_adb",
+    "/sys/devices/virtual/tty/ptmx",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p38", //system
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p36", // /cache	
+    "/sys/devices/virtual/misc/android_adb",
+    "/sys/devices/virtual/android_usb/android0/f_adb",
     "/sys/bus/usb",
-
-    // USB Drive is in here
-    "/sys/module/ehci_hcd*",
+	
+    // Mount persist and firmware
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0:0001/block/mmcblk0/mmcblk0p23", //persist
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0:0001/block/mmcblk0/mmcblk0p1", //firmware	
 
     NULL
 };
